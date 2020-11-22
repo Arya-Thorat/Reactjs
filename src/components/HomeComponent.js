b3d6b878048a047541e93e05f5d5e3c0884882ef
Arya Thorat <asthorat0401@gmail.com>
2020-11-22 11:26:00 +0530

    Week2 commit

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeComponent = (props) => {
+    return (
+        <div className="container">
+            <h4>HOME</h4>
+        </div>
+    );
+};
+
+export default HomeComponent;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,55 @@
+//container component
+import React , {Component} from 'react';
+import {DISHES} from '../shared/dishes';
+import MenuComponent from './MenuComponent';
+import DishDetail from './DishDetailComponent';
+import HeaderComponent from './HeaderComponent';
+import FooterComponent from './FooterComponent';
+import HomeComponent from './HomeComponent';
+import {Switch,Route,Redirect} from 'react-router-dom'; 
+
+class Main extends Component {
+  constructor(props)
+  {
+    super(props)
+    this.state={
+      dishes: DISHES,
+      // selectedDish:null //this is selectedDishId
+    }
+  }
+  // onDishSelect(dishId)
+  //   {
+  //       this.setState({
+  //           selectedDish : dishId
+  //       })
+  //   }
+  render(){
+    const HomePage=()=>{
+      return (
+        <HomeComponent/>
+      )
+    }
+
+    return (
+      <div>
+        <HeaderComponent/>
+        <Switch>
+        <Route path="/home" component={HomePage}/>
+        {/* this above method of calling a component wont pass props to that comp */}
+        <Route exact path="/menu" component={() => <MenuComponent dishes={this.state.dishes}/>}/>
+        <Redirect to="/home"/>
+        </Switch>
+        <FooterComponent/>
+        
+        {/* <MenuComponent dishes={this.state.dishes} onClick = {(dishId) => this.onDishSelect(dishId)}/>
+        //you can pass functions of the parent component aswell to the child to call whenever it deems fits
+        <DishDetail dish={this.state.dishes.filter(dish =>dish.id === this.state.selectedDish)[0]}/>
+        //the above passes the object of the dish that has been selected
+        //by selecting that object whose id matches the selectedDish  */}
+      
+      </div>
+    );
+  }
+}
+
+export default Main;
